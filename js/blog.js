8a67655c15c425497816e26288998b53ef8a526b
Charlie <charlieboyle252@gmail.com>
2024-03-25 22:22:06 -0500

    save and read blogs to local storage

--- js/blog.js
+++ js/blog.js
@@ -1,22 +1,19 @@
 const mode = document.getElementById("light-button");
 const heading = document.getElementById("title");
-const blog1 = document.getElementById("blog-1");
-const blog2 = document.getElementById("blog-2");
-const blog3 = document.getElementById("blog-3");
-const blog4 = document.getElementById("blog-4");
-const blog5 = document.getElementById("blog-5");
+const blogContainer = document.getElementById("blogContainer");
 const footer = document.getElementById("footer");
+
+function renderBlogs(){
+  let allBlogs = JSON.parse(localStorage.getItem("blogs"))
+  if (!allBlogs) { allBlogs = [] }
+}
 
 
 mode.addEventListener("click",() => {
     if (document.body.style.backgroundColor == "azure") {
         document.body.style.backgroundColor = "black";
         heading.style.color = "white";
-       blog1.style.color = "white";
-       blog2.style.color = "white";
-       blog3.style.color = "white";
-       blog4.style.color = "white";
-       blog5.style.color = "white";
+    
       footer.style.color = "white";
       
     }
@@ -24,11 +21,7 @@ mode.addEventListener("click",() => {
      document.body.style.backgroundColor == "black";
     document.body.style.backgroundColor = "azure";
     heading.style.color = "black";
-  blog1.style.color = "black";
-  blog2.style.color = "black";
-  blog3.style.color = "black";
-  blog4.style.color = "black";
-  blog5.style.color = "black";
+  
   footer.style.color = "black";
 
     }
--- js/form.js
+++ js/form.js
@@ -1,15 +1,17 @@
 
-function storageFunction(event){
+function storageFunction(event) {
     event.preventDefault();
 
 
     const username = document.getElementById("name").value;
     const title = document.getElementById("title").value;
     const content = document.getElementById("content").value;
+    const data = { username: username, title: title, content: content }
 
-    localStorage.setItem("username",username);
-    localStorage.setItem("title",title);
-    localStorage.setItem("content",content);
-
+    let allBlogs = JSON.parse(localStorage.getItem("blogs"))
+    if (!allBlogs) { allBlogs = [] }
+    allBlogs.push(data)
+console.log(allBlogs)
+    localStorage.setItem("blogs",JSON.stringify(allBlogs))
 }
 
