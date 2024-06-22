f6fe670fa3c36fb9d81cb6b230c2ca842e97b36d
Charlie <charlieboyle252@gmail.com>
2024-06-21 20:54:47 -0500

    finished renderBlogs function to append local storage data on blog page

--- js/blog.js
+++ js/blog.js
@@ -3,25 +3,48 @@ const heading = document.getElementById("title");
 const blogContainer = document.getElementById("blogContainer");
 const footer = document.getElementById("footer");
 
-function renderBlogs(){
+function renderBlogs() {
   let allBlogs = JSON.parse(localStorage.getItem("blogs"))
   if (!allBlogs) { allBlogs = [] }
+
+
+  blogContainer.innerHTML = ''
+
+  allBlogs.forEach(blog => {
+
+    const blogElement = document.createElement('div');
+
+    blogElement.innerHTML = `
+    <h2>By: ${blog.username}<h2>
+    <h2>${blog.title}</h2>
+    <p>${blog.content}</p>
+
+  `;
+
+    blogContainer.appendChild(blogElement);
+  });
+
 }
 
-mode.addEventListener("click",() => {
-    if (document.body.style.backgroundColor == "azure") {
-        document.body.style.backgroundColor = "black";
-        heading.style.color = "white";
-      footer.style.color = "white";
-      
-    }
-    else{
-     document.body.style.backgroundColor == "black";
+renderBlogs();
+
+mode.addEventListener("click", () => {
+  if (document.body.style.backgroundColor === "azure") {
+    document.body.style.backgroundColor = "black";
+    heading.style.color = "white";
+    footer.style.color = "white";
+    blogContainer.style.color = "white";
+
+
+
+  }
+  else {
+    document.body.style.backgroundColor === "black";
     document.body.style.backgroundColor = "azure";
     heading.style.color = "black";
-  footer.style.color = "black";
-
-    }
+    footer.style.color = "black";
+    blogContainer.style.color = "black";
+  }
 });
 
 
--- js/form.js
+++ js/form.js
@@ -1,17 +1,20 @@
-
 function storageFunction(event) {
     event.preventDefault();
 
-
-    const username = document.getElementById("name").value;
-    const title = document.getElementById("title").value;
-    const content = document.getElementById("content").value;
-    const data = { username: username, title: title, content: content }
-
-    let allBlogs = JSON.parse(localStorage.getItem("blogs"))
+    const username = document.getElementById("name")
+    const title = document.getElementById("title")
+    const content = document.getElementById("content")
+    const data = { username: username.value, title: title.value, content: content.value };
+    
+    let allBlogs = JSON.parse(localStorage.getItem("blogs"));
     if (!allBlogs) { allBlogs = [] }
-    allBlogs.push(data)
-console.log(allBlogs)
-    localStorage.setItem("blogs",JSON.stringify(allBlogs))
+
+    allBlogs.push(data);
+    console.log(allBlogs);
+    localStorage.setItem("blogs", JSON.stringify(allBlogs));
+    username.value = ''
+    title.value = ''
+    content.value = ''
 }
 
+document.getElementById("submit-button").addEventListener("click", storageFunction);
